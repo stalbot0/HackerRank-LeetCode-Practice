fd4c17a8b09b7a77965a9f9dccb0334c177d0825
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-06-09 18:00:53 -0500

    RPN problem solved

--- HR-LC-Java/src/main/java/LeetCode/JavaCode.java
+++ HR-LC-Java/src/main/java/LeetCode/JavaCode.java
@@ -1,7 +1,10 @@
 package LeetCode;
 
+import org.w3c.dom.Notation;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class JavaCode {
     public void rotateArray(int[] intArr, int k) {
@@ -22,5 +25,38 @@ public class JavaCode {
             listBeforeK.addAll(listAfterK);
             System.out.println(listBeforeK);
         }
+    }
+
+
+//    Evaluate the value of an arithmetic expression in Reverse Polish Notation. Valid operators are +, -, *, /.
+    public void reversePolishNotation(String[] tokens) {
+        String operators = "+-*/";
+        Stack<String> stackRPN = new Stack<>();
+
+        for (String string : tokens) {
+            if(!operators.contains(string)) {
+                stackRPN.push(string);
+            } else {
+                int x = Integer.parseInt(stackRPN.pop());
+                int y = Integer.parseInt(stackRPN.pop());
+                int index = operators.indexOf(string);
+
+                switch (index) {
+                    case 0:
+                        stackRPN.push(String.valueOf(x + y));
+                        break;
+                    case 1:
+                        stackRPN.push(String.valueOf(y - x));
+                        break;
+                    case 2:
+                        stackRPN.push(String.valueOf(y * x));
+                        break;
+                    case 3:
+                        stackRPN.push(String.valueOf(y / x));
+                        break;
+                }
+            }
+        }
+        System.out.println(Integer.valueOf(stackRPN.pop()));
     }
 }
--- HR-LC-Java/src/main/java/Main.java
+++ HR-LC-Java/src/main/java/Main.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        problem 1
-        JavaCode rotateArrObj = new JavaCode();
-        int[] rotateArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        int[] rotateArr2 = {};
-        rotateArrObj.rotateArray(rotateArr, 9);
-        rotateArrObj.rotateArray(rotateArr2, 4);
-        System.out.println();
+//        problem 1 (rotate array)
+//        JavaCode rotateArrObj = new JavaCode();
+//        int[] rotateArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+//        int[] rotateArr2 = {};
+//        rotateArrObj.rotateArray(rotateArr, 9);
+//        rotateArrObj.rotateArray(rotateArr2, 4);
+//        System.out.println();
+
+//        problem 2 (reverse polish notation)
+        JavaCode reversePolishNotationObj = new JavaCode();
+        String[] arrRPN = {"2", "1", "+", "3", "*"};
+        reversePolishNotationObj.reversePolishNotation(arrRPN);
     }
 }
